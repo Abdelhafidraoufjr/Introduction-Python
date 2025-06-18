7a1c08d77bb8ca2c657c9f6599db96b00caf12ec
adoulServer <adouladoul14@gmail.com>
2025-06-18 10:18:17 +0100

    day -1

---
+++ week -1/day -1/ex5.py
@@ -0,0 +1,16 @@
+
+my_fav_numbers = {2, 10, 20} 
+
+my_fav_numbers.add(25)
+my_fav_numbers.add(30)  
+  
+print(my_fav_numbers)
+
+my_fav_numbers.remove(30) 
+
+print(my_fav_numbers)
+
+friend_fav_numbers = {5, 13, 15}  
+
+All_numbers = my_fav_numbers.union(friend_fav_numbers)  
+print(All_numbers)
