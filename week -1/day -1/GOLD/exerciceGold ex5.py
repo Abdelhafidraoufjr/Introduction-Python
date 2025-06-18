70877c2d9cb4ccb5b63590d15b344289a9d1bbac
adoulServer <adouladoul14@gmail.com>
2025-06-18 11:38:24 +0100

    gold -5

---
+++ week -1/day -1/GOLD/exerciceGold ex5.py
@@ -0,0 +1,13 @@
+number1 = int(input("the 1st number: "))
+number2 = int(input("the 2nd number: "))
+number3 = int(input("the 3rd number: "))
+
+numbers_list = [number1, number2, number3]
+
+print(f"Input the 1st number: {number1}")
+print(f"Input the 2nd number: {number2}")
+print(f"Input the 3rd number: {number3}")
+
+print("Liste des nombres :", max(numbers_list))
+
+
