1b58eaf968b60d4cd3d25fa5baf0fbfc8ededf20
adoulServer <adouladoul14@gmail.com>
2025-06-18 12:08:44 +0100

    defi -1

---
+++ week -1/day -1/DEFI/defi -1.py
@@ -0,0 +1,9 @@
+number = int(input("Entre un nombre : "))
+length = int(input("Entre la longueur : "))
+
+results = []
+
+for i in range(1, length + 1):
+    results.append(number * i)
+
+print(results)
