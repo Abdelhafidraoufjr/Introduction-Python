eed0fb3afc36ab99ea376a5fa4f0a898f825cbd5
adoulServer <adouladoul14@gmail.com>
2025-06-19 17:31:29 +0100

    anniv

--- week -1/day -2/exercice xp2.py
+++ week -1/day -2/exercice xp2.py
@@ -1,9 +1,7 @@
-# Provided family data
 family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
 
 total_cost = 0
 
-# Loop through the dictionary and calculate prices
 for name, age in family.items():
     if age < 3:
         price = 0
@@ -15,5 +13,4 @@ for name, age in family.items():
     print(f"{name.capitalize()} (age {age}) ticket price: ${price}")
     total_cost += price
 
-# Print the total cost
 print(f"\nTotal cost for the whole family: ${total_cost}")
