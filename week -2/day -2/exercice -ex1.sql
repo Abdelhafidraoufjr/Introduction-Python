c0d1d1c4ae19e825cb280d4ee30f8fcbb31522ed
adoulServer <adouladoul14@gmail.com>
2025-06-23 12:02:05 +0100

    ex -1

---
+++ week -2/day -2/exercice -ex1.sql
@@ -0,0 +1,7 @@
+select * from items order by price DESC;
+
+select * from items where price >= 80 ORDER by price DESC;
+
+select first_name, last_name from customers order by first_name ASC LIMIT 3;
+
+select last_name from customers order by last_name DESC;
