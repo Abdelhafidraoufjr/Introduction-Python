a0cb321ab7e6aa66c1b665a6dc2eac9e620e755b
adoulServer <adouladoul14@gmail.com>
2025-06-24 18:53:39 +0100

    actors_db

--- week -2/day -1/DEFI/Acteurs.sql
+++ week -2/day -1/DEFI/Acteurs.sql
@@ -7,16 +7,16 @@ CREATE TABLE actors(
 )
 
 INSERT INTO actors (first_name, last_name, age, number_oscars) VALUES
-('Meryl',      'Streep',      '1949-06-22', 3),  -- 3 Oscars
-('Daniel',     'Day-Lewis',   '1957-04-29', 3),  -- 3 Oscars
-('Denzel',     'Washington',  '1954-12-28', 2),  -- 2 Oscars
-('Frances',    'McDormand',   '1957-06-23', 4),  -- 4 Oscars (dont 1 productrice)
-('Leonardo',   'DiCaprio',    '1974-11-11', 1),  -- 1 Oscar
-('Cate',       'Blanchett',   '1969-05-14', 2),  -- 2 Oscars
-('Mahershala', 'Ali',         '1974-02-16', 2),  -- 2 Oscars
-('Tom',        'Hanks',       '1956-07-09', 2),  -- 2 Oscars
-('Natalie',    'Portman',     '1981-06-09', 1),  -- 1 Oscar
-('Joaquin',    'Phoenix',     '1974-10-28', 1);  -- 1 Oscar
+('Meryl',      'Streep',      '1949-06-22', 3),  
+('Daniel',     'Day-Lewis',   '1957-04-29', 3), 
+('Denzel',     'Washington',  '1954-12-28', 2), 
+('Frances',    'McDormand',   '1957-06-23', 4), 
+('Leonardo',   'DiCaprio',    '1974-11-11', 1),  
+('Cate',       'Blanchett',   '1969-05-14', 2),  
+('Mahershala', 'Ali',         '1974-02-16', 2),  
+('Tom',        'Hanks',       '1956-07-09', 2),  
+('Natalie',    'Portman',     '1981-06-09', 1), 
+('Joaquin',    'Phoenix',     '1974-10-28', 1);  
 
 SELECT COUNT(*) FROM actors
 
---
+++ week -2/day -2/readme.md
@@ -0,0 +1,6 @@
+# Schéma de la base de données
+
+Voici le schéma simplifié de la base de données avec les entités **Actors**:
+
+
+![Schéma base de données Actors](images/actors.png)
